f37084124016a5c0c10e3144832808c4f2e5c1fc
Braden Kelly <braden.kelly@nwciowa.edu>
2016-11-19 14:37:29 -0600

    Edited Authentication methods.

--- Ecommerce Project/App_Start/Startup.Auth.cs
+++ Ecommerce Project/App_Start/Startup.Auth.cs
@@ -13,38 +13,46 @@ namespace Ecommerce_Project
 {
     public partial class Startup {
 
+        //Taken from https://www.asp.net/web-api/overview/security/external-authentication-services#GOOGLE
+        public const string ExternalCookieAuthenticationType = CookieAuthenticationDefaults.ExternalAuthenticationType;
+        public const string ExternalOAuthAuthenticationType = "ExternalToken";
+
+        static Startup()
+        {
+            PublicClientId = "self";
+
+            IdentityManagerFactory = new IdentityManagerFactory(IdentityConfig.Settings, () => new IdentityStore());
+
+            CookieOptions = new CookieAuthenticationOptions();
+
+            OAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = "/Token",
+                AuthorizeEndpointPath = "/api/Account/ExternalLogin",
+                Provider = new ApplicationOAuthProvider(PublicClientId, IdentityManagerFactory, CookieOptions)
+            };
+        }
+
+        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+
+        public static CookieAuthenticationOptions CookieOptions { get; private set; }
+
+        public static IdentityManagerFactory IdentityManagerFactory { get; set; }
+
+        public static string PublicClientId { get; private set; }
+
+
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301883
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
-            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+            // Enable the application to use cookies to authenticate users
+            app.UseCookieAuthentication(CookieOptions);
 
-            // Enable the application to use a cookie to store information for the signed in user
-            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
-            // Configure the sign in cookie
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login"),
-                Provider = new CookieAuthenticationProvider
-                {
-                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-                        validateInterval: TimeSpan.FromMinutes(30),
-                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
-                }
-            });
-            // Use a cookie to temporarily store information about a user logging in with a third party login provider
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            // Enable the application to use a cookie to store temporary information about a user logging in with a third party login provider
+            app.UseExternalSignInCookie(ExternalCookieAuthenticationType);
 
-            // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
-            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
-
-            // Enables the application to remember the second login verification factor such as phone or email.
-            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
-            // This is similar to the RememberMe option when you log in.
-            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+            // Enable the application to use bearer tokens to authenticate users
+            app.UseOAuthBearerTokens(OAuthOptions, ExternalOAuthAuthenticationType);
 
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
@@ -52,18 +60,14 @@ namespace Ecommerce_Project
             //    clientSecret: "");
 
             //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
+            //    consumerKey: "",
+            //    consumerSecret: "");
 
             //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+            //    appId: "",
+            //    appSecret: "");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            //app.UseGoogleAuthentication();
         }
     }
 }
